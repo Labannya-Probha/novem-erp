6cd56ca45034c6e6f835f3d061319b50cb99a4e1
Ankur382 <85364373+Ankur382@users.noreply.github.com>
2026-06-14 19:58:55 +0600

    Update roles.js

--- src/lib/roles.js
+++ src/lib/roles.js
@@ -11,6 +11,7 @@ export const NAV_ACCESS = {
   reservations: ['MANAGER', 'FRONT_OFFICE'],
   calendar: ['MANAGER', 'FRONT_OFFICE'],
   nightaudit: ['MANAGER', 'FRONT_OFFICE'],
+  housekeeping: ['MANAGER', 'FRONT_OFFICE', 'HOUSEKEEPING'],
   pos: ['MANAGER', 'RESTAURANT', 'FRONT_OFFICE'],
   facilities: ['MANAGER', 'FRONT_OFFICE', 'RESTAURANT'],
   inventory: ['MANAGER', 'STORE'],
